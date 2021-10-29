18d39cf93cef3b1141dbfdd3be1d5147e61bd4a7
KooMinJun <mjkoo711@gmail.com>
2021-10-30 00:48:30 +0900

    set scrollTabBar

---
+++ tempalarm/View/AlarmView.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmView.swift
+//  tempalarm
+//
+//  Created by 구민준 on 2021/10/30.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    var body: some View {
+      VStack {
+        //todo
+        Color.green
+      }
+      .frame(maxWidth: .infinity)
+    }
+}
+
+struct AlarmView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmView()
+    }
+}
---
+++ tempalarm/View/SettingView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingView.swift
+//  tempalarm
+//
+//  Created by 구민준 on 2021/10/30.
+//
+
+
+import SwiftUI
+
+struct SettingView: View {
+  var body: some View {
+    VStack {
+      // todo
+      Color.blue
+    }
+    .frame(maxWidth: .infinity)
+  }
+}
+
+struct SettingView_Previews: PreviewProvider {
+  static var previews: some View {
+    SettingView()
+  }
+}
